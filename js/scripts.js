8ab2c69896576f62d2747c70c918edbd285af3e6
akash-vartak <akashvartak.1995@gmail.com>
2016-11-15 23:28:29 +0530

    adding full template

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function(){
+	
+	
+	
+/* FLEX SLIDER */
+	var $flexSlider = $('.flexslider');
+	$flexSlider.flexslider({
+		animation: "slide",
+		controlsContainer: ".flex-container",
+		prevText: "&larr;",
+		nextText: "&rarr;",
+		controlNav: false,
+directionNav: true,
+		before:	function($slider){
+			$slider.find('.flex-caption').fadeOut('fast');			
+		},
+		after: function($slider){
+			$slider.find('.flex-caption').fadeIn();			
+		},
+		
+	});
+	
+	
+  $(window).load(function() {
+    $('.flexslider').flexslider({
+			animation: "slide",
+          controlsContainer: ".flex-container"
+	});
+  });
+	
+	/* prettyPhoto */
+	
+	$("a[rel^='prettyPhoto']").prettyPhoto({
+		
+		social_tools: false,
+		
+		
+	});
+	
+});
+
+//End document.ready
